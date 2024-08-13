879818f1fad0e912622fddd8ae097e38fb318492
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2024-08-13 16:55:47 +0500

    core autoconfig added

---
+++ src/main/java/uz/pdp/springframeworkcore/Main.java
@@ -0,0 +1,13 @@
+package uz.pdp.springframeworkcore;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import uz.pdp.springframeworkcore.config.MyConfig;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext container = new AnnotationConfigApplicationContext(MyConfig.class);
+        MyBean2 myBean2 = container.getBean(MyBean2.class);
+        myBean2.hi();
+
+    }
+}
