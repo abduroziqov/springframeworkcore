00dd0a68931bb00a888fcd961df66e0491a14f1c
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2024-08-19 11:51:16 +0500

    core_java_config done advanced way

--- src/main/java/uz/pdp/springframeworkcore/Main.java
+++ src/main/java/uz/pdp/springframeworkcore/Main.java
@@ -1,6 +1,5 @@
 package uz.pdp.springframeworkcore;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
